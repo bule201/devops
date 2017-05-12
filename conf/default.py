0bf6381a398cf89b6785c27f058807fe3f3c09bc
bule201 <253114517@qq.com>
2017-05-12 14:37:08 +0800

    v1

--- conf/default.py
+++ conf/default.py
@@ -22,10 +22,10 @@ from django.conf.global_settings import *
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'devops'
-APP_TOKEN = '3add5b98-4a83-46a5-9743-7fdfb8cb1ab8'
+APP_ID = 'uwei'
+APP_TOKEN = '1858cf15-7dc9-4899-be40-6559af637b60'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://paas.xkops.com'
+BK_PAAS_HOST = 'http://paas.bking.com'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
